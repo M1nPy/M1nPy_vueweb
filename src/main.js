778fee32072799d0190195645fc7a5ba8e1d24ce
M1nPy <sundayoblivion@gmail.com>
2021-03-07 17:56:54 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -2,19 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import * as Sentry from '@sentry/vue'
-import { Integrations } from '@sentry/tracing'
-
-Sentry.init({
-  Vue,
-  dsn:
-    'https://800e4a2ccd1b4a3cba7fd5a4e6eed53a@o534280.ingest.sentry.io/5653256',
-  integrations: [new Integrations.BrowserTracing()],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0
-})
 
 Vue.config.productionTip = false
 
